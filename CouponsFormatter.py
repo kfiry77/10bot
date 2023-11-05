ca6121dc77c91fda5458ad050c9c2515e74ecbfe
kfiry77 <kfir.ye@gmail.com>
2023-11-06 00:52:25 +0200

    Fixed some formatting warnings, and removed unused method.

--- CouponsFormatter.py
+++ CouponsFormatter.py
@@ -1,5 +1,4 @@
 import base64
-from datetime import date
 import requests
 
 HTML_ROW_TEMPLATE = """
@@ -12,50 +11,46 @@ HTML_ROW_TEMPLATE = """
     """
 HTML_PAGE_TEMPLATE = """
         <!DOCTYPE html>
-        <html>
-        <head>
-        <style>
-	@page {{ margin:0 }}	
-        #barcodes {{
-        font-family: Arial, Helvetica, sans-serif;
-        border-collapse: collapse;
-        }}
-        img {{
-        opacity: 1
-        }}
-        #barcodes td, #barcodes th {{
-        border: 1px solid #ddd;
-        padding: 8px;
-        }}
-        #barcodes tr:nth-child(even){{
-        background-color: #f2f2f2;}}
-
-        #barcodes tr:hover {{background-color: #ddd;}}
-
-        #barcodes th {{
-        padding-top: 12px;
-        padding-bottom: 12px;
-        vertical-align: top;
-        text-align: center;
-        background-color: #04AA6D;
-        color: white;
-        }}
-        </style>
-        </head>
+        <html> 
+        <head> <style>
+            @page {{ margin:0 }}	
+            #barcodes {{
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            }}
+            img {{
+            opacity: 1
+            }}
+            #barcodes td, #barcodes th {{
+            border: 1px solid #ddd;
+            padding: 8px;
+            }}
+            #barcodes tr:nth-child(even){{
+            background-color: #f2f2f2;}}
+            #barcodes tr:hover {{background-color: #ddd;}}
+            #barcodes th {{
+            padding-top: 12px;
+            padding-bottom: 12px;
+            vertical-align: top;
+            text-align: center;
+            background-color: #04AA6D;
+            color: white;
+            }}
+        </style> </head>
         <body>
             <h1> {restaurantName} </h1>
             <table id="barcodes">
             <tr> 
-  				<th width="10%">Order date</th>
-				<th width="10%">$$$</th>
-				<th width="10%">Barcode</th> 
-				<th width="70%">Barcode image</th>
-             </tr>
+                <th width="10%">Order date</th>
+                <th width="10%">$$$</th>
+                <th width="10%">Barcode</th> 
+                <th width="70%">Barcode image</th>
+            </tr>
             {output_table}
             </table>
         </body>
         </html>
-    """
+"""
 
 
 class CouponFormatter:
@@ -71,6 +66,7 @@ class CouponFormatter:
             # Fetch the image from the URL
             response = requests.get(image_url)
 
+            image_data = ''
             if response.status_code == 200:
                 # Get the image content and encode it in base64
                 image_data = base64.b64encode(response.content).decode("utf-8")
@@ -78,14 +74,7 @@ class CouponFormatter:
                 print(f'fail to get image:{image_url}')
 
             output_table += HTML_ROW_TEMPLATE.format(order_date=coupon['Date'],
-                                                     barcode_number=coupon['barcode'].replace("-", "-<br>" ),
+                                                     barcode_number=coupon['barcode'].replace("-", "-<br>"),
                                                      image_data=image_data,
                                                      amount=coupon['amount'])
         return HTML_PAGE_TEMPLATE.format(output_table=output_table, restaurantName=restaurant_name)
-
-    def write_to_files(self):
-        for r in self.coupons.values():
-            filename = f"output/{date.today().strftime('%y-%m-%d')}_{r['vendorName']}.html"
-            with open(filename, 'w', encoding='utf-8') as file:
-                file.write(self.format_orders(r['orders'], r['restaurantName']))
-            HTML(string=open(filename, 'rb').read()).write_pdf(filename + ".pdf")
