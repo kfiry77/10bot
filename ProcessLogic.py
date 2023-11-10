58ca25ac0f0d77f7d8621128c6afbbf6a22e2c83
kfiry77 <kfir.ye@gmail.com>
2023-11-10 20:53:18 +0200

    Added Logic to skip publishing, if the current report is the same as previous, that were sent.

--- ProcessLogic.py
+++ ProcessLogic.py
@@ -1,3 +1,4 @@
+from PickleSerializer import PickleSerializer
 from TenbisLogic import Tenbis
 from Processor import *
 
@@ -8,12 +9,43 @@ class ProcessLogic(Processor):
         self.ten_bis = Tenbis(args)
         self.publishers = publishers
 
+    @staticmethod
+    def compare_coupons_files(c1, c2):
+        if len(c1) != len(c2) or c1.keys() != c2.keys():
+            return False
+
+        for k in c1.keys():
+            l1 = c1[k]['orders']
+            l2 = c2[k]['orders']
+
+            if len(l2) != len(l1):
+                return False
+
+            coupons_list1 = sorted([item['barcode'] for item in l1])
+            coupons_list2 = sorted([item['barcode'] for item in l1])
+            if coupons_list1 != coupons_list2:
+                return False
+
+        return True
+
     def process_impl(self, data):
+        send_report = False
         budget_available = self.ten_bis.is_budget_available()
-        print('budget available=', budget_available)
+        print('Budget available=', budget_available)
         if budget_available:
             self.ten_bis.buy_coupon(40)
+            send_report = True
         coupons = self.ten_bis.get_unused_coupons()
+        coupons_pickle = PickleSerializer('coupons')
+        if coupons_pickle.exists():
+            prev_coupons = coupons_pickle.load()
+            send_report = send_report or not self.compare_coupons_files(prev_coupons, coupons)
+        else:
+            send_report = True
+        coupons_pickle.create(coupons)
+        if not send_report:
+            print('No report changes, publish will be skipped')
+            return
         for vendor_coupon in coupons.values():
             for p in self.publishers:
                 p.process(vendor_coupon)
